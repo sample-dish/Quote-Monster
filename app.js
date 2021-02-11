383804edef6389e0e4f847cbca36d4725822c9b5
Amelia Hanson <ameliahanson@Amelias-MacBook-Pro.local>
2021-02-10 17:12:26 -0800

    Put quotes on page

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ $(document).ready(function(){
     $(".button-container").on("click",async function(){
         $(".words").slideUp().animate({margin:"0", padding:"0"})
 
-        var response = await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/")
+        var response = await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random/")
         var parsedResponse = await response.json()
-        
 
-        $(".quote-container").animate({height:"0.5px", paddingTop:"0", paddingBottom: "0"},350).animate({height: "100%", paddingTop:"1px", paddingBottom: "1px"},350)
+        $("#quote-text").text(parsedResponse.data[0].quoteText)
+        $("#quote-name").text("-"+parsedResponse.data[0].quoteAuthor)
+
+        /* var lengthOfList = parsedResponse.data.length
+
+        var randomNumber=Math.floor(Math.random()*lengthOfList)
+
+        console.log(parsedResponse.data[randomNumber]) */
+
+        $(".quote-container").animate({height:"0.5px", paddingTop:"0", paddingBottom: "0"},350).animate({height: "100%", paddingTop:"1px", paddingBottom: "1px"},400)
         $(".words").animate({marginTop:"30px", marginBottom: "30px"}).slideDown(1000)
     })
 })
